b1346ea20d05a82d46c07f7540dc237b4f7a408d
Luke Robert Kakad <lukek@r0login0.localdomain.hpc.udel.edu>
2026-05-05 12:37:52 -0400

    beans

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 #define NUMPLANETS      8
-#define MINUTE 			60L
+#define MINUTE 			60
 #define HOUR 			MINUTE*60
 #define DAY 			HOUR*24
 #define WEEK 			DAY*7
@@ -13,7 +13,7 @@
 #define MAX_DISTANCE 5000.0
 #define MAX_VELOCITY 50000.0
 #define MAX_MASS 938e18  //approximate mass of ceres.
-#define DURATION (10L*YEAR)
+#define DURATION (10*YEAR)
 #define INTERVAL DAY
 //End Configurable
 
